7c931493b5db202176776c1cb3b71d77ef912e7b
Amogh <142930584+amogh344@users.noreply.github.com>
2024-04-22 22:38:11 +0530

    Add files via upload
    Firat Program

---
+++ Read-and-print-integer.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class GFG
+{
+    public static void main(String[] args) {
+        int num;
+        System.out.println("Enter the Integer:");
+        Scanner s=new Scanner(System.in);
+        num=s.nextInt();
+        System.out.println("Entered Integer is:"+num);
+        s.close();
+
+    }
+}
