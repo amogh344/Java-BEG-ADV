68427a5234036f6500c38e0034ead8179de98b8d
Amogh <142930584+amogh344@users.noreply.github.com>
2024-07-21 16:02:04 +0530

    Update lab2.java

--- lab2.java
+++ lab2.java
@@ -1,53 +1,36 @@
-package amogh;
 import java.util.*;
-class TensPlaceComp implements Comparator<Integer>
-{
-	@Override
-	public int compare(Integer n1,Integer n2)
-	{
-		int tp1=(n1%100)/10;
-		int tp2=(n2%100)/10;
-		
-		return Integer.compare(tp1,tp2);
-	}
-}
-
 public class lab2
 {
-	public static void main(String []args)
-	{
-		int min=10,max=99,count=10;
-		ArrayList<Integer> rNumbers= new ArrayList<>();
-		Random r=new Random();
-		for(int i=0;i<count;i++)
-		{
-			int rNum;
-			do
-			{
-				rNum = r.nextInt((max-min)+1)+min;
-			}while (rNum%2!=0||rNum%5!=0);
-			
-			rNumbers.add(rNum);
-		}
-		System.out.println("Unsorted Numbers:");
-		{
-			for(int num :rNumbers)
-			{
-				System.out.print(num+" ");
-			}
-			System.out.println();
-		}
-		rNumbers.sort(new TensPlaceComp());
-		System.out.println("Sorted Numbers:");
-		{
-			for(int num :rNumbers)
-			{
-				System.out.print(num+" ");
-			}
-		}
-		
-		}
-		
+    public static void main(String[] args)
+    {
+        int min = 10;
+        int max = 100;
+        int count = 10;
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        Random r = new Random();
+        while (numbers.size() < count)
+        {
+            int num = min + r.nextInt(max - min + 1);
+            if (num % 2 == 0 && num % 5 == 0)
+            {
+                numbers.add(num);
+            }
+        }
+
+        System.out.println("Generated Numbers: " + numbers);
+
+        Collections.sort(numbers, new Comparator<Integer>()
+        {
+            @Override
+            public int compare(Integer n1, Integer n2)
+            {
+                int t1 = (n1 / 10) % 10;
+                int t2 = (n2 / 10) % 10;
+                return Integer.compare(t1, t2);
+            }
+        });
+
+        System.out.println("Sorted Numbers: " + numbers);
+    }
 }
-
-
